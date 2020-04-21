acf77a01b4941289e6213a769d1e37cfcf1bf09d
Tomas Madeja <t.mm.madeja@gmail.com>
2020-04-22 01:04:50 +0200

    add participant

--- src/main/cz/muni/fi/pv204/host/SecureChannel.java
+++ src/main/cz/muni/fi/pv204/host/SecureChannel.java
@@ -1,24 +1,41 @@
 package cz.muni.fi.pv204.host;
 
 
+import org.apache.groovy.json.internal.ArrayUtils;
+import org.bouncycastle.crypto.CryptoException;
+import sun.security.util.ArrayUtil;
+
 import javax.smartcardio.CardException;
 import javax.smartcardio.CommandAPDU;
 import javax.smartcardio.ResponseAPDU;
 import java.security.SecureRandom;
+import java.security.spec.ECPoint;
 
 public class SecureChannel {
 
     public static final short CHALLANGE_LENGTH = 32;
+    public static final int PASS_LEN = 4;
 
     private JCardSymInterface channel;
     private MagicAes aes;
     private SecureRandom rand;
+    private Participant participant;
 
-    public SecureChannel(JCardSymInterface channel) throws Exception {
+    public SecureChannel(
+            JCardSymInterface channel,
+            String participantId,
+            char[] password
+    ) throws Exception {
+        if (password.length != PASS_LEN) throw new Exception();
+        for (char c : password) {
+            if (! Character.isDigit(c)) throw new Exception();
+        }
+
         this.channel = channel;
 
         rand = SecureRandom.getInstanceStrong();
         aes = new MagicAes();
+        participant = new Participant(participantId, password);
     }
 
     public void establishSC() throws Exception {
@@ -32,17 +49,26 @@ public class SecureChannel {
     public void unwrap() {}
 
     private ResponseAPDU establishmentRound1() throws CardException {
-        short sizeOfGx = 1;
-        short sizeOfAB = 1;
-        short sizeOfZKP = 1;
-        short sizeOfID = 1;
+
+        Round1Payload round1 = participant.createRound1PayloadToSend();
+        byte[] Gx1 = round1.getGx1().getEncoded(false);
+        byte[] Gx2 = round1.getGx2().getEncoded(false);
+        byte[] zkp1 = combine(
+                round1.getKnowledgeProofForX1().getV().getEncoded(false),
+                round1.getKnowledgeProofForX1().getr().toByteArray()
+        );
+        byte[] zkp2 = combine(
+                round1.getKnowledgeProofForX2().getV().getEncoded(false),
+                round1.getKnowledgeProofForX2().getr().toByteArray()
+        );
+        byte[] participantIDA = round1.getParticipantId().getBytes();
+
+        short sizeOfGx = (short) Gx1.length;
+        short sizeOfZKP = (short) zkp1.length;
+        short sizeOfID = (short) participantIDA.length;
         // Obtained from generate round 1
+
         // TODO call round 1 generation
-        byte[] Gx1 = new byte[0];
-        byte[] Gx2 = new byte[0];
-        byte[] zkp1 = new byte[0];
-        byte[] zkp2 = new byte[0];
-        byte[] participantIDA = new byte[0];
 
         int outgoingLength = 2*sizeOfGx + 2*sizeOfZKP + sizeOfID;
         byte[] outgoing = new byte[
@@ -87,7 +113,7 @@ public class SecureChannel {
 
     private ResponseAPDU establishmentRound3(
             ResponseAPDU response
-    ) throws CardException {
+    ) throws CardException, CryptoException {
         short sizeOfGx = 1;
         short sizeOfZKP = 1;
         short sizeOfAB = 1;
@@ -147,15 +173,28 @@ public class SecureChannel {
                 sizeOfID
         );
 
-        // TODO CALL VALIDATION
+
+        participant.validateRound2PayloadReceived(
+                new Round2Payload(
+                        new String(participantIDB),
+                        participant.ecCurve.decodePoint(Gx3),
+                        participant.ecCurve.decodePoint(Gx4),
+                        participant.ecCurve.decodePoint(B),
+                        new SchnorrZKP(),
+                        new SchnorrZKP(),
+                        new SchnorrZKP()
+            )
+        );
 
         // Response
-
         // Obtained from generate round 3
-        // TODO call round 3 generation
-        byte[] A = new byte[0];
-        zkp1 = new byte[0];
-        byte[] participantIDA = new byte[0];
+        Round3Payload round3 = participant.createRound3PayloadToSend();
+        byte[] A = round3.getA().getEncoded(false);
+        zkp1 = combine(
+                round3.getKnowledgeProofForX2s().getV().getEncoded(false),
+                round3.getKnowledgeProofForX2s().getr().toByteArray()
+        );
+        byte[] participantIDA = round3.getParticipantId().getBytes();
 
         int outgoingLength = 2*sizeOfGx + 2*sizeOfZKP + sizeOfID;
         byte[] outgoing = new byte[
@@ -196,7 +235,7 @@ public class SecureChannel {
         }
 
         // TODO add call for keying material
-        byte[] keyingMaterial = new byte[CHALLANGE_LENGTH];
+        byte[] keyingMaterial = participant.calculateKeyingMaterial().getEncoded(false);
 
         aes.generateKey(keyingMaterial, challenge);
 
@@ -232,6 +271,13 @@ public class SecureChannel {
                 throw new Exception(); // TODO throw something
             }
         }
+    }
+
+    private byte[] combine(byte[] a, byte[] b) {
+        byte[] r = new byte[a.length + b.length];
+        System.arraycopy(a, 0, r, 0, a.length);
+        System.arraycopy(b, 0, r, a.length, b.length);
+        return r;
     }
 
 }
